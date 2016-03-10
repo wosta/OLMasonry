2e9d16a05c45d1aa642ca6613e322731051c11be
旺仔 <weiwang1202@126.com>
2016-03-10 13:58:44 +0800

    Initial Commit

---
+++ OLMasonry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OLMasonry
+//
+//  Created by 魏旺 on 16/3/10.
+//  Copyright © 2016年 olive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OLMasonry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OLMasonry
+//
+//  Created by 魏旺 on 16/3/10.
+//  Copyright © 2016年 olive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
